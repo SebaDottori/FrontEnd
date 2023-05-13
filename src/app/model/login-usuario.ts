981f6a26402b3b3c1a335cb518d53ba879240664
Seba Dottori <sebadottori@outlook.com>
2023-05-13 11:55:13 -0300

    Initial commit

---
+++ src/app/model/login-usuario.ts
@@ -0,0 +1,9 @@
+export class LoginUsuario {
+    nombreUsuario: string;
+    password: string;
+
+    constructor(nombreUsusario: string, password: string){
+        this.nombreUsuario = nombreUsusario;
+        this.password = password;
+    }
+}
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { NuevoUsuario } from '../model/nuevo-usuario';
+import { Observable } from 'rxjs';
+import { JwtDto } from '../model/jwt-dto';
+import { LoginUsuario } from '../model/login-usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  authURL = 'http://localhost:8080/auth/';
+
+  constructor(private HttpCLient: HttpClient) { }
+
+  public nuevo(nuevoUsuario: NuevoUsuario): Observable<any> {
+    return this.HttpCLient.post<any>(this.authURL + 'nuevo', nuevoUsuario);
+  }
+
+  public login(loginUsuario: LoginUsuario): Observable<JwtDto>{
+    return this.HttpCLient.post<JwtDto>(this.authURL + 'login', loginUsuario);
+  }
+}
